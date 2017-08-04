cf3244a8a3438f2af104a8688c66c7fbc7c682f5
Leticia Han <lbhan94@gmail.com>
2017-08-04 11:40:32 -0700

    add hover effect - black

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
+  // create grid (16x16)
   var $grid = $('.grid');
-
   for (i = 0; i < 16; i++) {
     var row = '<div class="row">';
-
     for (j = 0; j < 16; j++) {
       row += '<div class="square"></div>';
     }
-
     row += '</div>';
     $grid.append(row);
   }
-  
+  // make height=width
   var $squareWidth = $('.square').width();
   $('.square').css({
     'height': $squareWidth + 'px'
   });
+
+  // create hover effect: color-black;
+  $('.square').hover(function() {
+    $(this).css({'background-color': '#000'});
+  })
 
 });
