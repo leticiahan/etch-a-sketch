1b1b1fdcf4008701cf0bca9e6669694db92dda2a
Leticia Han <lbhan94@gmail.com>
2017-08-07 22:09:59 -0700

    add clear button function

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 $(document).ready(function() {
 
-  // create grid (16x16)
-  var $grid = $('.grid');
-  for (i = 0; i < 16; i++) {
-    var row = '<div class="row">';
-    for (j = 0; j < 16; j++) {
-      row += '<div class="square" data-count="0"></div>';
-    }
-    row += '</div>';
-    $grid.append(row);
-  }
+  var num = 16; //default
+  createGrid(num);
 
-  // make height=width
-  var $squareWidth = $('.square').width();
-  $('.square').css({
-    'height': $squareWidth + 'px'
+  // create grid (16x16)
+  function createGrid(num) {
+    var $grid = $('.grid');
+    for (i = 0; i < num; i++) {
+      var row = '<div class="row">';
+      for (j = 0; j < num; j++) {
+        row += '<div class="square" data-count="0"></div>';
+      }
+      row += '</div>';
+      $grid.append(row);
+    }
+
+    // make height=width
+  /*  $('.square').css({
+      'width': $('.row').width() / num + '%'
+    });*/
+    var $rowWidth = $('.row').width();
+    console.log($rowWidth);
+    var $squareWidth = $rowWidth / num;
+    console.log($squareWidth);
+    $('.square').css({
+      'width': $squareWidth + 'px',
+      'height': $squareWidth + 'px'
+    });
+    /*var $squareWidth = $('.square').width();
+    $('.square').css({
+      'height': $squareWidth + 'px'
+    });*/
+
+    // hover effect with gradient (black)
+    $('.square').hover(function() {
+        $(this).addClass('black');
+        var count = parseInt($(this).data('count'), 10) + 1;
+        $(this).data('count', count);
+        var opacity = 0.1;
+        if($(this).data('count') <= 10) {
+        	$(this).css({
+          'opacity': opacity * $(this).data('count')
+          });
+        }
+      });
+
+  };
+
+  // clear button
+  $('.clear').click(function() {
+    $('.grid').replaceWith('<div class="grid"></div>');
+    createGrid(num);
   });
 
-  // hover effect with gradient (black)
-  $('.square').mouseover(function() {
-      $(this).addClass('black');
-      var count = parseInt($(this).data('count'), 10) + 1;
-      $(this).data('count', count);
-      var opacity = 0.1;
-      if($(this).data('count') <= 10) {
-      	$(this).css({
-        'opacity': opacity * $(this).data('count')
-        });
-      }
-    });
+
 
 
 });
